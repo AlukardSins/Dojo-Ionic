f6148b8464ff5d35592d3dcbd0f3cb2e1a29d443
Daniel Rodriguez <gameover_lifetime@outlook.com>
2017-08-26 10:03:52 -0500

    First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { GithubProvider } from '../../providers/github/github';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  informacion:any = []
+
+  constructor(private _github:GithubProvider){
+
+  }
+
+  funcionDojo(){
+    var promise = this._github.buscarUsuario('AlukardSins')
+    promise.then(data=>{
+      this.informacion=data
+      console.log(data)
+    })
+  }
+
+}
